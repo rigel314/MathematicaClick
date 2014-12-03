b07b30f939a57b0ba786e45b35f3e3d96b12150c
Cody Creager <pi.rubiks@gmail.com>
2014-12-02 22:04:30 -0800

    Finally vaugly working.

--- MathematicaClick.py
+++ MathematicaClick.py
@@ -1,31 +1,37 @@
 import sublime, sublime_plugin, time
 
-class MathematicaClickSelectCommand(sublime_plugin.TextCommand):
-	# def __init__(self, view):
-	# 	self.view = view
-	# 	self.thing = 0
-	# 	self.time = time.time()
-	def run(self, edit):
-		# self.thing = time.time() - self.time
-		# self.time = time.time()
-		# if self.thing > 1:
-		# 	self.thing = 0
+class MathematicaClickSelectCommand(sublime_plugin.TextCommand) :
+	def run(self, edit) :
+		reg = self.view.sel()[0]
+		i = reg.a
+		j = reg.b
 
-		for region in self.view.sel():
-			sublime.status_message(str(region.a))
+		if i == j :
+			self.view.sel().add(self.view.word(reg))
+			return
 
-		# if self.thing == 0:
-		# 	view.run_command("drag_select")
-		# 	sublime.status_message("blar")
-		# else:
-		# 	sublime.status_message("test"+str(self.thing))
+		if i > 1 :
+			i = self.search('.([{"\'+-', i-2, -1)
+		if j < self.view.size()-1 :
+			j = self.search('.)]}"\'+-', j+1, 1)
 
-class ClickDetector(sublime_plugin.EventListener):
-	def __init__(self):
+		self.view.sel().add(sublime.Region(i, j))
+
+	def search(self, chars, start, step) :
+		i = start
+		while 1 :
+			for char in chars :
+				if self.view.substr(i-1) == char or i == 0 or i == self.view.size() :
+					return i;
+			i = i+step
+
+
+class ClickDetector(sublime_plugin.EventListener) :
+	def __init__(self) :
 		self.time = time.time()
-	def on_text_command(self, view, name, args):
+	def on_text_command(self, view, name, args) :
 		if name == 'drag_select':
 			thing = time.time() - self.time
 			self.time = time.time()
-			if thing < 1:
+			if thing < .5:
 				return ('mathematica_click_select')
